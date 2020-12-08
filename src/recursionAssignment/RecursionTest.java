fdbfbc6b78dc838955f8514a4f5ea31b02dbdf65
Engelbert Aroozoo <engelbert.aroozoo21@ycdsbk12.ca>
2020-12-08 23:02:34 +0000

    Count7 RED

--- src/recursionAssignment/RecursionTest.java
+++ src/recursionAssignment/RecursionTest.java
@@ -1,8 +1,8 @@
 package recursionAssignment;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Before;
-import org.junit.jupiter.api.After;
+import org.junit.Before;
+import org.junit.After;
 
 import recursionAssignment.Recursion;
 
@@ -21,6 +21,7 @@ public class RecursionTest{
         // make assertion statement(s)
         // assertEquals(EXPECTEDVALUE, TESTVALUE);
         // assertTrue(TESTBOOLEAN);
+        assertEquals(Recursion.count7(7), 1);
     }
     
     // add more tests
